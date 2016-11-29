223853f8ea7f83615c2edd5614ddaff1b562ded0
Debra Handley <dmorgan000@yahoo.com>
2016-11-28 20:27:10 -0600

    Create 2 books in script.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,26 @@
 // Put all of your jQuery and JavaScript in this document.
+var book1 = {
+    "id": 1,
+    "name": "Lasagna: A Retrospective",
+    "author": "Garfield"
+    "picture_url": "http://graphics8.nytimes.com/images/2015/10/15/dining/15RECIPE20DIN/15RECIPE20DIN-articleLarge.jpg",
+    "price": 24,
+    "selling_points": [
+        "Lasagna is delicious.",
+        "The essential guide to Italian casseroles of all types.",
+        "Real G's move silent, like Lasagna. -Lil Wayne"
+    ]
+}
+
+var book2 = {
+    "id": 2,
+    "name": "How Many Hot Wings are Too Many?",
+    "author": "Jane Smith"
+    "picture_url": "https://images-na.ssl-images-amazon.com/images/I/51je868YWML._SY160_.jpg",
+    "price": 24,
+    "selling_points": [
+        "Hot wings have no pork.",
+        "Hot wings heat up wellthe nest day.",
+        "Real G's move silent, like Lasagna. -Lil WayneEverybody loves hot wings!"
+    ]
+}
